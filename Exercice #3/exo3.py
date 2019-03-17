f86046b38e18bac8d5bb5cc955aa47ed37e9f26f
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2019-03-17 08:56:26 +0100

    Update exo3.py

--- Exercice #3/exo3.py
+++ Exercice #3/exo3.py
@@ -4,6 +4,7 @@ Created on Sat Mar 16 09:27:11 2019
 
 @author: Admin
 """
+import numpy as np
 import nltk
 import string
 import random
@@ -36,9 +37,8 @@ def text2sentences2(path):
     return sentences
 
 train2 = text2sentences2(rep+'train_none_original.txt')
-s2 = train2[0]
+s2 = train2[5]
 s3 = s2.split("\t")
-#s4 = train2[1].split("\t")
 
 utt = s3[0]
 cor = s3[1]
@@ -76,8 +76,9 @@ utt_t  = []
 for ans in answers :
     utt_t.append(ans)
 n = len(utt_t[-1])
-utt_t[-1] = utt_t[-1][0:(n-2)]
+utt_t[-1] = utt_t[-1][0:(n-2)] #remove /n
 utt_t.append(utt[1:len(utt)]) #utterance of user at last
+user_response = utt_t 
 
 corpus = ['This is the first document.','This document is the second document.','this is the third document.']
 vectorizer = TfidfVectorizer()
@@ -92,8 +93,9 @@ req_tfidf = flat[-2]
 def response(user_response):
     robo_response=''
     #sent_tokens.append(user_response)
-    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
-    tfidf = TfidfVec.fit_transform(utt_t)
+    #TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize)
+    tfidf = TfidfVec.fit_transform(user_response)
     vals = cosine_similarity(tfidf[-1], tfidf)
     idx=vals.argsort()[0][-2]
     flat = vals.flatten()
@@ -101,10 +103,44 @@ def response(user_response):
     req_tfidf = flat[-2]
     if(req_tfidf==0):
         robo_response=robo_response+"I am sorry! I don't understand you"
-        return robo_response
+        return 55
     else:
         robo_response = robo_response+utt_t[idx]
-        return robo_response
+        return idx
+
+# la phrase correcte et est l'avant dernière phrase de utt_t
+# i.e. idx = len(utt_t) -2 : 19
+        
+##besoin du contexte pour la réponse
+def user_response(data):
+    all_resp = []
+    for i_dial in range(len(data)):
+        dial = data[i_dial]
+        dial = dial.split("\t")
+        utt = dial[0]
+        answers = dial[3].split("|") 
+        resp  = []
+        
+        for ans in answers :
+            resp.append(ans)
+            
+        n = len(resp[-1])
+        resp[-1] = resp[-1][0:(n-2)] #remove /n
+        resp.append(utt[1:len(utt)]) #utterance of user at last
+        all_resp.append(resp)
+    return all_resp
+
+d = user_response(train2[0:100])   
+res = []    
+for sent in d:
+    idx = response(sent)
+    if (idx == 19): #correct answer
+        res.append(True)
+    else:
+        res.append(False)
+ 
+sum(res)/100 #bad
+res[5]
 
 flag=True
 print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
@@ -126,6 +162,6 @@ while(flag==True):
         flag=False
         print("ROBO: Bye! take care..")
         
-user_response = answers       
+     
 
 # source : https://medium.com/analytics-vidhya/building-a-simple-chatbot-in-python-using-nltk-7c8c8215ac6e
